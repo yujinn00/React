fe39bab40dfa02f99e382c09eed9b3309814bb22
yujin <qrt365@naver.com>
2024-04-10 00:39:34 +0900

    Section 6

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,13 +7,15 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 setInterval(() => {
-  ReactDOM.render(
+  root.render(
     <React.StrictMode>
-      <CommentList />
-    </React.StrictMode>,
-    document.getElementById('root')
+      <NotificationList />
+    </React.StrictMode>
   );
 }, 1000);
 
