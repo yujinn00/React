3fc806d131b246138bbeba80098d1bae0bcf40e4
yujin <qrt365@naver.com>
2024-05-04 19:14:42 +0900

    Section 7

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,16 +8,15 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <NotificationList />
-    </React.StrictMode>
-  );
-}, 1000);
+root.render(
+  <React.StrictMode>
+    <Accommodate />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
